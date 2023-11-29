5f9f712fedcd61fc07a8b290410c74b91cdd4bae
nayarasaffa <nayara.saffa.tif421@polban.ac.id>
2023-11-29 09:26:33 +0700

    final

--- frontend/src/components/sidebar/components/Brand.js
+++ frontend/src/components/sidebar/components/Brand.js
@@ -1,10 +1,11 @@
 import React from "react";
+import logosi from "../../../assets/logo_si.png";
 
 // Chakra imports
 import { Flex, useColorModeValue } from "@chakra-ui/react";
 
 // Custom components
-import { HorizonLogo } from "components/icons/Icons";
+// import { HorizonLogo } from "components/icons/Icons";
 import { HSeparator } from "components/separator/Separator";
 
 export function SidebarBrand() {
@@ -13,7 +14,13 @@ export function SidebarBrand() {
 
   return (
     <Flex align="center" direction="column">
-      <HorizonLogo h="26px" w="175px" my="32px" color={logoColor} />
+      {/* <HorizonLogo h="26px" w="175px" my="32px" color={logoColor} /> */}
+      <img
+        src={logosi}
+        height="0px"
+        width="175px"
+        style={{ marginBottom: "32px" }}
+      />
       <HSeparator mb="20px" />
     </Flex>
   );
